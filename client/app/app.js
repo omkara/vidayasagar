97e9c00d015643123be2f2e6ed7d282ba2c630af
Dwivedi <shashank.dwivedi@hpe.com>
2017-02-07 11:24:18 +0530

    [client] Basic implementation of client

---
+++ client/app/app.js
@@ -0,0 +1,55 @@
+/*parent module definitions*/
+
+angular.module('vidyasagarApp', ['ui.router', 'ngResource'])	
+	   .config(function($stateProvider, $urlRouterProvider) {  
+	       	$urlRouterProvider.otherwise('/home');  		
+    		$stateProvider
+    		.state('home', {
+            	url: '/home',
+            	templateUrl: 'app/home/partial-home.html'
+        	})
+        	.state('about', {
+            	url: '/about',
+            	views: {
+					// the main template will be placed here (relatively named)
+            		'': { templateUrl: 'app/about/partial-about.html' },
+            		'columnOne@about': { template: 'Look I am a column!' }
+            		// for column two, we'll define a separate controller 
+            		'columnTwo@about': { 
+                		templateUrl: 'app/table-data.html',
+                		controller: 'scotchController'
+            		}
+        		} 
+        	})
+        	.state('contact', {
+            	url: '/contact',
+            	templateUrl: 'app/contact/partial-contact.html',
+            	controller: 'scotchController'  
+        	});
+
+        
+		});
+
+angular.module('vidyasagarApp')
+		.controller('scotchController', function($scope) {
+    
+    		$scope.message = 'test';
+   
+    		$scope.scotches = [
+        		{
+            		name: 'Macallan 12',
+            		price: 50
+        		},
+        		{
+            		name: 'Chivas Regal Royal Salute',
+            		price: 10000
+        		},
+        		{
+            		name: 'Glenfiddich 1937',
+            		price: 20000
+        		}
+    		];
+    
+		});
+
+		
